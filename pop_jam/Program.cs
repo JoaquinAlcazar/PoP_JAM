75c07f452625504190202443021b16eb0b4c9455
joaquinalcazar <joaquin.alcazar.7e4@itb.cat>
2024-01-22 12:02:21 +0100

    Refactorizado

--- pop_jam/Program.cs
+++ pop_jam/Program.cs
@@ -8,14 +8,13 @@ public class PopProject
 {
     public static void Main(string[] args)
     {
-        int dia;
-        int mes;
-        int any;
+        int dia = 3;
+        int mes = 1;
+        int any = 2024;
         bool validat;
         Console.WriteLine("Introdueix el dia, mes i any");
         validat = valida(dia, mes, any);
-        Console.WriteLine("El format no és correcte");
-        Console.WriteLine("La data és correcta {1}", validat);
+        Console.WriteLine(valida(dia, mes, any) ? "La data és correcta" : "El format no és correcte");
     }
 
     public static bool valida(int day, int month, int year)
@@ -31,53 +30,38 @@ public class PopProject
 
         // determinem la quantitat de dies del mes:
 
-        int totalDaysMonth = 0;
+        int totalDaysMonth = 30;
 
         switch (month)
         {
+            case 2: // verifica l'any de traspàs
+
+                if (year % 4 == 0)
+                {
+                    totalDaysMonth = 29;
+                }
+                else totalDaysMonth = 28;
+                break;
 
             case 1:
 
-            case 3:
+            case 3: 
 
-            case 5:
+            case 5: 
 
-            case 7:
+            case 7: 
 
-            case 8:
+            case 8: 
 
-            case 10:
+            case 10: 
 
             case 12: totalDaysMonth = 31; break;
 
-            case 4:
-
-            case 6:
-
-            case 9:
-
-            case 11: totalDaysMonth = 30; break;
-
-            case 2: // verifica l'any de traspàs
-
-                if ((year % 400 == 0) ||
-
-                ((year % 4 == 0) && (year % 100 != 0)))
-                    totalDaysMonth = 29;
-
-                else totalDaysMonth = 28;
-
-                break;
-
         }
 
-        if (day > totalDaysMonth)
-
-            return false;
+        if (day > totalDaysMonth) return false;
 
         else return true;
 
     }
 }
-Program.cs
-Program.cs s'està mostrant.
