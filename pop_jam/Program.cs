c0d4f91769d27970395b230e081f707694ee3b26
joaquinalcazar <joaquin.alcazar.7e4@itb.cat>
2024-01-22 11:49:11 +0100

    commiting

--- pop_jam/Program.cs
+++ pop_jam/Program.cs
@@ -1,42 +1,83 @@
-﻿namespace PP2M03M05
+﻿
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Security.Cryptography;
+namespace PoPRefactoring;
+
+public class PopProject
 {
-    public class provaPractica
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        int dia;
+        int mes;
+        int any;
+        bool validat;
+        Console.WriteLine("Introdueix el dia, mes i any");
+        validat = valida(dia, mes, any);
+        Console.WriteLine("El format no és correcte");
+        Console.WriteLine("La data és correcta {1}", validat);
+    }
+
+    public static bool valida(int day, int month, int year)
+    {
+
+        if (day < 1 || day > 31)
+
+            return false;
+
+        if (month < 1 || month > 12)
+
+            return false;
+
+        // determinem la quantitat de dies del mes:
+
+        int totalDaysMonth = 0;
+
+        switch (month)
         {
-            string passwordInput;
 
-            Console.WriteLine("Introduce contraseña");
-            passwordInput = Console.ReadLine();
+            case 1:
 
-            Console.WriteLine(passwordChecker(passwordInput) ? "Es correcte" : "No es correcte");
+            case 3:
+
+            case 5:
+
+            case 7:
+
+            case 8:
+
+            case 10:
+
+            case 12: totalDaysMonth = 31; break;
+
+            case 4:
+
+            case 6:
+
+            case 9:
+
+            case 11: totalDaysMonth = 30; break;
+
+            case 2: // verifica l'any de traspàs
+
+                if ((year % 400 == 0) ||
+
+                ((year % 4 == 0) && (year % 100 != 0)))
+                    totalDaysMonth = 29;
+
+                else totalDaysMonth = 28;
+
+                break;
+
         }
 
-        public static bool passwordChecker(string password)
-        {
+        if (day > totalDaysMonth)
 
-            char[] invalidChars = { '*', 'ñ', '#', '@', '€', '%', '&', '¡', '!', '?', '¿' };
-            char actualLetter;
-            char specialTester;
-            int charCounter = 0;
-            foreach (char c in password)
-            {
-                actualLetter = c;
-                foreach (char c2 in invalidChars)
-                {
-                    specialTester = c2;
-                    if (actualLetter == specialTester)
-                    {
-                        return false;
-                    }
-                }
-                charCounter++;
-            }
-            if (charCounter < 5 || charCounter > 12)
-            {
-                return false;
-            }
-            return true;
-        }
+            return false;
+
+        else return true;
+
     }
 }
+Program.cs
+Program.cs s'està mostrant.
